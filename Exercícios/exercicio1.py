29a4f311813149c678110bba31bf6fa65117e6ef
Ana Forte <anapatriciabf20082gmail.com>
2025-10-03 15:28:16 +0100

    Ex1

---
+++ Exercícios/exercicio1.py
@@ -0,0 +1,10 @@
+#Exercicio1: Converter Segundos em Horas, Minutos e Segundos
+#Enunciado: Desenvolva um programa que assuma uma entrada em segundos e a converta para horas, minutos e segundos
+
+total_segundos = int(input("Insira segundos: "))
+
+horas = total_segundos / 3600
+
+minutos = total_segundos 
+
+print("Horas: {horas}, Minutos: {}, Segundos: {}")
